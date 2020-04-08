d996dc63fa2ee2ed056689c7e1fd8f4f78979bc6
Amanda Yip <amandayip@cmail.carleton.ca>
2020-04-08 17:47:45 -0400

    Add files via upload

---
+++ colordb.js
@@ -0,0 +1,29 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/colordb";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  console.log("Database created!");
+  db.close();
+});
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("colordb");
+  dbo.createCollection("colors", function(err, res) {
+    if (err) throw err;
+    console.log("Collection created!");
+    db.close();
+  });
+});
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("colordb");
+  var myobj = { name: "Company Inc", address: "Highway 37" };
+  dbo.collection("customers").insertOne(myobj, function(err, res) {
+    if (err) throw err;
+    console.log("1 document inserted");
+    db.close();
+  });
+});
