103b9adb685924a4a5f0181805bc595af2a33e99
Amanda Yip <amandayip@cmail.carleton.ca>
2020-04-08 16:20:08 -0400

    Add files via upload

---
+++ colorarray.js
@@ -0,0 +1,8 @@
+var colors = [];
+while (colors.length < 100) {
+    do {
+        var color = Math.floor((Math.random()*1000000)+1);
+    } while (colors.indexOf(color) >= 0);
+    colors.push("#" + ("000000" + color.toString(16)).slice(-6));
+}
+console.log(colors);
